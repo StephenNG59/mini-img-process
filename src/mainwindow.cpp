8132e54a89edcdeff5def5e63ecff155dd27a78f
StephenNG59 <15920902266@139.com>
2019-07-09 10:51:51 +0800

    Added basic framework

---
+++ src/mainwindow.cpp
@@ -0,0 +1,50 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    pixmap_GSW = new QPixmap(QString::fromUtf8("./img/a.png"));
+//    ui->label_imgFrame->setPixmap(*pixmap_GSW);
+    ui->label_imgFrame->setScaledContents(true);
+
+    connect(ui->pushButton_test, SIGNAL(clicked()), this, SLOT(on_pushButton_test_clicked()));
+    connect(ui->actionOpen_Image, SIGNAL(triggered()), this, SLOT(on_actionOpen_Image_clicked()));
+
+//    pixmap_GSW.trueMatrix()
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionOpen_Image_clicked()
+{
+    fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "/home", tr("Images (*.png, *.jpg)"));
+
+    QMovie *move = new QMovie(fileName);
+    ui->label_imgFrame->setMovie(move);
+    move->start();
+}
+
+void MainWindow::on_pushButton_test_clicked()
+{
+    QImage *img = new QImage(fileName);
+    QImage *changed_img = new QImage(*img);
+    for (int i = 0; i < 100; i++)
+        for (int j = 0; j < 100; j++)
+        {
+            QRgb rgb = img->pixel(i, j);
+            int r = 0.5 * qRed(rgb), g = 0.5 * qGreen(rgb), b = 0.5 * qBlue(rgb);
+            changed_img->setPixel(i, j, qRgb(r, g, b));
+        }
+
+    ui->label_imgFrame->setPixmap(QPixmap::fromImage(*changed_img));
+//    pixmap_GSW = new QPixmap(QString::fromUtf8("./img/a.png"));
+//    ui->label_imgFrame->setPixmap(*pixmap_GSW);
+    ui->pushButton_close->setText("Test Done");
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QPixmap>
+#include <QMovie>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    QPixmap *pixmap_GSW;
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QString fileName;
+private slots:
+    // self-defined slots
+    void on_actionOpen_Image_clicked();
+    void on_pushButton_test_clicked();
+};
+
+#endif // MAINWINDOW_H
