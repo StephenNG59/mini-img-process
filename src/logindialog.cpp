af94c64c52fa9503c60858d5b645d5a7817d15d7
陈科皓 <1271370893@qq.com>
2019-07-11 15:37:53 +0800

    added time show

--- src/logindialog.cpp
+++ src/logindialog.cpp
@@ -1,11 +1,17 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
+#include <QTimer>
+#include <QDateTime>
 
 LoginDialog::LoginDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
+    QTimer *timer = new QTimer(this);
+    //关联定时器溢出信号和相应的槽函数
+    connect(timer, SIGNAL(timeout()), this, SLOT(timerUpdate()));
+    timer->start(1000);
 }
 
 LoginDialog::~LoginDialog()
@@ -27,4 +33,14 @@ void LoginDialog::on_loginbtn_clicked()
     ui->usrLineEdit->clear();
     ui->pwdLineEdit->clear();
     ui->usrLineEdit->setFocus();
+}
+
+void LoginDialog::timerUpdate()
+{
+   //获取系统现在的时间
+   QDateTime time = QDateTime::currentDateTime();
+   //设置系统时间显示格式
+   QString str = time.toString("yyyy-MM-dd hh:mm:ss dddd");
+   //在标签上显示时间
+   ui->lineEdit->setText(str);
 }
--- src/logindialog.h
+++ src/logindialog.h
@@ -18,6 +18,7 @@ public:
 
 private slots:
     void on_loginbtn_clicked();
+    void timerUpdate();
 
 private:
     Ui::LoginDialog *ui;
